c6cc10529d03eba32fa54d549807bc73b0f0690a
Hoan Nguyen <hoan.nguyen@dinosys.vn>
2018-01-23 11:50:35 +0700

    Add timezone

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module RailsDevise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Hanoi'
   end
 end
