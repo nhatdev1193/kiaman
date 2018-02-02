9fb55960ed80106a077372e7d7e62109b5a3e23a
Dang Nguyen <pandabamboo90@gmail.com>
2018-02-03 00:06:54 +0700

    Remove unused views

--- app/controllers/admin/admin_devise/passwords_controller.rb
+++
@@ -1,2 +0,0 @@
-class Admin::AdminDevise::PasswordsController < Devise::PasswordsController
-end
--- app/controllers/admin/admin_devise/registrations_controller.rb
+++
@@ -1,2 +0,0 @@
-class Admin::AdminDevise::RegistrationsController < Devise::RegistrationsController
-end
