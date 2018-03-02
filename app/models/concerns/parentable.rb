c6a165efa6eae7cc2d8b6b767dd655fa15f3848c
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-02 23:40:40 +0700

    Create parentable concern module

---
+++ app/models/concerns/parentable.rb
@@ -0,0 +1,37 @@
+module Parentable
+  extend ActiveSupport::Concern
+
+  included do
+    # Associations
+    has_many :children, class_name: 'Organization', foreign_key: 'parent_id'
+    belongs_to :parent, class_name: 'Organization', optional: true
+
+    # Callbacks
+    before_save :set_level
+
+    # Scopes
+    scope :can_become_parent, lambda do |current_model|
+      if current_model.id.nil?
+        all
+      else
+        where.not(id: current_model.id) # Not itself
+            .where('level <= ?', current_model.level) # Must be at higher level
+      end
+    end
+  end
+
+
+  #
+  # Methods defined here are going to extend the class, not the instance of it
+  #
+  class_methods do
+  end
+
+
+  # Private methods
+  private
+
+  def set_level
+    self.level = parent.nil? ? 1 : parent.level + 1
+  end
+end
