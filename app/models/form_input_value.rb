2e787af902b5131b035a7b37823b3347914d64f0
Trong Thao <trongthaonh@gmail.com>
2018-03-04 19:14:07 +0700

    Remove disused model

--- app/models/form_input_value.rb
+++
@@ -1,7 +0,0 @@
-class FormInputValue < SoftDeleteBaseModel
-  belongs_to :contract
-  belongs_to :form
-  belongs_to :form_input_kind
-
-  validates :contract_id, :form_id, :form_input_kind_id, :form_input_condition_ids, presence: true
-end
