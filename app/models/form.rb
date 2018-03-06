962475f34f4903c418d4e358eeca2f91b68753cc
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-07 00:19:19 +0700

    Add show, edit, action and rename text to vietnamese

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,9 +1,12 @@
 class Form < SoftDeleteBaseModel
+
+  # Associations
   has_many :steps
   has_many :form_fields
   has_many :form_values
   belongs_to :contract_kind, optional: true
 
+  # Validations
   validates :name, presence: true, uniqueness: true
   validates :is_template, presence: true
 end
