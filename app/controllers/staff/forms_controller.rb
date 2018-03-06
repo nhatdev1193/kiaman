e45287b1352bc057c3d66dd24541f3f780bc08b7
Trong Thao <trongthaonh@gmail.com>
2018-03-06 22:09:53 +0700

    Render form by form_fields

--- app/controllers/staff/forms_controller.rb
+++ app/controllers/staff/forms_controller.rb
@@ -1,6 +1,6 @@
 class Staff::FormsController < Staff::BaseController
   def view
-    @form = Form.find(params[:id])
+    @form = Form.includes(form_fields: :form_input).find(params[:id])
   end
 
   def execute; end
