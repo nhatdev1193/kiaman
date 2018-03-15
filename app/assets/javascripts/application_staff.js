fe4caf337d773913e9a61d01ccf5c8c753184d09
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-16 01:36:47 +0700

    Add method to pagniate the result using will_paginate

--- app/assets/javascripts/application_staff.js
+++ app/assets/javascripts/application_staff.js
@@ -23,10 +23,16 @@
 let ready = function () {
   $('.select2').select2();
 
-  //Flat red color scheme for iCheck
+  // Flat red color scheme for iCheck
   $('input[type="checkbox"].i-check, input[type="radio"].i-check').iCheck({
     checkboxClass: 'icheckbox_square-green',
     radioClass   : 'iradio_square-green'
-  })
+  });
+
+  // Changing per page
+  $('.will_paginate_per_page').change(function () {
+    let $t = $(this);
+    window.location.href = replaceUrlParam(window.location.toString(), 'per_page', $t.val());
+  });
 };
 document.addEventListener('turbolinks:load', ready);
