b9d0cadaf211ffeae49e384d90fb31fab10196a8
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-16 01:47:20 +0700

    Add the link to view user detail

--- app/controllers/staff/people_controller.rb
+++ app/controllers/staff/people_controller.rb
@@ -5,7 +5,7 @@ class Staff::PeopleController < Staff::BaseController
   def index
     service = PersonDataService.new
     @people_steps = service.person_list
-    @people_steps = @people_steps.paginate
+                           .paginate(page: params[:page], per_page: params[:per_page])
   end
 
   def new
