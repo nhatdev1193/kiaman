b977df9af287a2827a0fe0e72e1da438b96649e9
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-18 17:31:57 +0700

    Configure Devise to allow required params for editting user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :mobile_phone, :password, :password_confirmation])
+  end
 
   def after_sign_in_path_for(resource)
     staff_dashboard_url
