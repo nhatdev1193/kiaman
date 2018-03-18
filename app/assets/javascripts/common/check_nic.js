6cce62beaaee31c2b612bd8b52d3624af35f2d92
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-18 23:28:54 +0700

    Fixed the fast prospect + normal prospect form with validation, fixed broken routes and refactored check_nic method

--- app/assets/javascripts/common/check_nic.js
+++ app/assets/javascripts/common/check_nic.js
@@ -2,33 +2,34 @@
 $(function () {
   $('.btn-check-nic-number').click(function () {
     let $t = $(this),
-        nicNumber = $('#person_nic_number').val(),
-        productId = $('#person_product_id').val(),
-        $nicMessage = $('#nic-message'),
-        $personForm = $('#new_person');
+        nicNumber = $('.form-control[name="person[nic_number]"]').val(),
+        productId = $('.form-control[name="person[product_id]"]').val(),
+        $nicMessage = $('#nic_message'),
+        $form = $('#normal_prospect_form');
 
     if (!productId) {
-      alert('Xin hãy chọn loại sản phẩm')
-    } else {
-      $.ajax({
-        url: '/staff/people/nic_check',
-        type: 'post',
-        data: {
-          nic_number: nicNumber,
-          product_id: productId
-        },
-        success: function (res) {
-          if (parseInt(res.code) === 409) {
-            $personForm.find(':submit').attr('disabled', true);
-            $t.parents('.form-group').addClass('has-error');
-          } else if (parseInt(res.code) === 200) {
-            $personForm.find(':submit').attr('disabled', false);
-            $t.parents('.form-group').addClass('has-success');
-          }
-
-          $nicMessage.html(res.message);
-        }
-      })
+      alert('Xin hãy chọn loại sản phẩm');
+      return false;
     }
+
+    $.ajax({
+      url: '/staff/people/nic_check',
+      type: 'post',
+      data: {
+        nic_number: nicNumber,
+        product_id: productId
+      },
+      success: function (res) {
+        if (parseInt(res.code) === 409) {
+          $form.find(':submit').attr('disabled', true);
+          $t.parents('.form-group').addClass('has-error');
+        } else if (parseInt(res.code) === 200) {
+          $form.find(':submit').attr('disabled', false);
+          $t.parents('.form-group').addClass('has-success');
+        }
+
+        $nicMessage.html(res.message);
+      }
+    });
   });
 });
