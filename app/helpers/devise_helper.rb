68a8b2b459bf4f41afa8c2fa03b7af565bd7e2a1
Dang Nguyen <pandabamboo90@gmail.com>
2018-03-18 23:29:26 +0700

    Update devise helper, replace li with div

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg) }.join
     html = <<-HTML
     <div class="alert alert-danger alert-dismissible" role="alert">
       <button type="button" class="close" data-dismiss="alert">
